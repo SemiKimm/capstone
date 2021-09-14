3704788d4e7fe06732d187a3c016ad98c7bbbf0a
SemiKimm <semikimm.m@gmail.com>
2021-09-15 00:44:33 +0900

    [semi-commit] PostingActivity.java & PostingRequest.java & Posting.xml : Posting function LostPostActivity.java & lostpost.xml : Post function PostListActivity.java & PostRequest.java & post_list.xml & post_list_item.xml : Post List function and db data check SelectPostingActivity.java & select_posting.xml : select lostposting or getposting LostPostingActivity.java & LostPostingRequest.java & lost_posting.xml : 분실물 글 작성 기능 GetPostingActivity.java & GetPostingRequest.java & get_posting.xml : 습득물 글 작성 기능 SelectSearchPostActivity.java & select_search_post.xml : 하단바 search button 클릭시 렌더링, 검색하려는 물건이 분실물인지 습득물인지 선택 SearchGetPostActivity.java & search_get_post.xml : 습득물 검색 기능 SearchLostPostActivity.java & search_lost_post.xml : 분실물 검색 기능 -------------------------------------- [change] search function 추가

---
+++ capstone2/app/src/main/java/com/example/capstone2/SearchLostPostActivity.java
@@ -0,0 +1,115 @@
+package com.example.capstone2;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class SearchLostPostActivity extends AppCompatActivity {
+    private TextView date, color, category;
+
+    String[] colorItems = {"검정색 ","흰색","빨강색","연두색","파랑색 ","노랑색","핑크색","보라색","회색"};
+    String[] categoryItems = {"가방", "의류", "전자제품", "악세서리", "모자", "신발", "시계", "휴대폰"};
+
+    //날짜 선택 구현
+    DatePickerDialog.OnDateSetListener dateSetListener1=
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int yy, int mm, int dd) {
+                    //DatePicker 선택한 날짜를 TextView 에 설정
+                    TextView dateView=findViewById(R.id.searchLostDateData1);
+                    dateView.setText(String.format("%d/%d/%d",yy,mm+1,dd));
+                }
+            };
+
+    DatePickerDialog.OnDateSetListener dateSetListener2=
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int yy, int mm, int dd) {
+                    //DatePicker 선택한 날짜를 TextView 에 설정
+                    TextView dateView=findViewById(R.id.searchLostDateData2);
+                    dateView.setText(String.format("%d/%d/%d",yy,mm+1,dd));
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_lost_post);
+
+        Button homeButton=findViewById(R.id.homeBtn);
+        homeButton.setOnClickListener(view -> {
+            Intent intent=new Intent(SearchLostPostActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        Button searchButton=findViewById(R.id.searchBtn);
+        searchButton.setOnClickListener(view -> {
+            Intent intent=new Intent(SearchLostPostActivity.this, SelectSearchPostActivity.class);
+            startActivity(intent);
+        });
+
+        Button postingButton=findViewById(R.id.postingBtn);
+        postingButton.setOnClickListener(view -> {
+            Intent intent=new Intent(SearchLostPostActivity.this, SelectPostingActivity.class);
+            startActivity(intent);
+        });
+
+        //colorSpinner 처리 (색상선택처리)
+        Spinner colorSpin = (Spinner)findViewById(R.id.searchLostColorSpinner);
+        color=findViewById(R.id.searchLostColorData);
+        ArrayAdapter<String> colorAdapter=new ArrayAdapter<String>(
+                this,android.R.layout.simple_spinner_item,colorItems
+        );
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        colorSpin.setAdapter(colorAdapter);
+        colorSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override//선택되면
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                color.setText(colorItems[position]);
+            }
+            @Override//아무것도 선택 안되면
+            public void onNothingSelected(AdapterView<?> parent) {
+                color.setText("색상선택");
+            }
+        });
+
+        //categorySpinner 처리 (색상선택처리)
+        Spinner categorySpin = (Spinner)findViewById(R.id.searchLostCategorySpinner);
+        category=findViewById(R.id.searchLostCategoryData);
+        ArrayAdapter<String> categoryAdapter=new ArrayAdapter<String>(
+                this,android.R.layout.simple_spinner_item,categoryItems
+        );
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        categorySpin.setAdapter(categoryAdapter);
+        categorySpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override//선택되면
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                category.setText(categoryItems[position]);
+            }
+            @Override//아무것도 선택 안되면
+            public void onNothingSelected(AdapterView<?> parent) {
+                category.setText("물건 분류 선택");
+            }
+        });
+
+        //날짜 선택 구현
+        Button dateBtn1=findViewById(R.id.searchSelectDateBtn1);
+        dateBtn1.setOnClickListener(view -> {
+            Calendar calender = Calendar.getInstance();
+            new DatePickerDialog(this, dateSetListener1, calender.get(Calendar.YEAR), calender.get(Calendar.MONTH), calender.get(Calendar.DATE)).show();});
+
+        Button dateBtn2=findViewById(R.id.searchSelectDateBtn2);
+        dateBtn2.setOnClickListener(view -> {
+            Calendar calender = Calendar.getInstance();
+            new DatePickerDialog(this, dateSetListener2, calender.get(Calendar.YEAR), calender.get(Calendar.MONTH), calender.get(Calendar.DATE)).show();});
+    }
+}
