34d2aee36fc8fc15cb1ae2d2a72ecd613b201cb1
SemiKimm <semikimm.m@gmail.com>
2021-09-15 23:29:53 +0900

    [semi-commit] GetPosting.php & LostPosting.php : 데이터베이스에 분실물 및 습득물 게시글 데이터 INSERT GetPostGetJson.php & LostPostGetJson.php : 데이터베이스에서 분실물 및 습득물 게시글 데이터 가져오기 SELECT getjson.php & Posting.php : sample(구현 테스트용) ------------------------------------ [change] local & category data 추가

--- postingphp/GetPosting.php
+++ postingphp/GetPosting.php
@@ -3,15 +3,17 @@
     mysqli_query($con,'SET NAMES utf8');
  
     $GetPostTitleData = $_POST["GetPostTitleData"];
+    $GetPostCategoryData = $_POST["GetPostCategoryData"];
+    $GetPostLocalData = $_POST["GetPostLocalData"];
     $GetPostPlaceData = $_POST["GetPostPlaceData"];
     $GetPostDateData = $_POST["GetPostDateData"];
     $GetPostColorData = $_POST["GetPostColorData"];
     $GetPostMoreInfoData = $_POST["GetPostMoreInfoData"];
     $GetPostImgData = $_POST["GetPostImgData"];
 
-        $statement = mysqli_prepare($con, "INSERT INTO GETPOST(GetPostTitleData, GetPostPlaceData, GetPostDateData, GetPostColorData, GetPostMoreInfoData, GetPostImgData) VALUES (?,?,?,?,?,?)");
-    mysqli_stmt_bind_param($statement, "ssssss"
-        ,$GetPostTitleData, $GetPostPlaceData, $GetPostDateData, $GetPostColorData, $GetPostMoreInfoData, $GetPostImgData);
+        $statement = mysqli_prepare($con, "INSERT INTO GETPOST(GetPostTitleData, GetPostCategoryData, GetPostLocalData, GetPostPlaceData, GetPostDateData, GetPostColorData, GetPostMoreInfoData, GetPostImgData) VALUES (?,?,?,?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "ssssssss"
+        ,$GetPostTitleData, $GetPostCategoryData, $GetPostLocalData, $GetPostPlaceData, $GetPostDateData, $GetPostColorData, $GetPostMoreInfoData, $GetPostImgData);
     mysqli_stmt_execute($statement);
  
     $response = array();
--- postingphp/LostPosting.php
+++ postingphp/LostPosting.php
@@ -3,15 +3,17 @@
     mysqli_query($con,'SET NAMES utf8');
  
     $LostPostTitleData = $_POST["LostPostTitleData"];
+    $LostPostCategoryData = $_POST["LostPostCategoryData"];
+    $LostPostLocalData = $_POST["LostPostLocalData"];
     $LostPostPlaceData = $_POST["LostPostPlaceData"];
     $LostPostDateData = $_POST["LostPostDateData"];
     $LostPostColorData = $_POST["LostPostColorData"];
     $LostPostMoreInfoData = $_POST["LostPostMoreInfoData"];
     $LostPostImgData = $_POST["LostPostImgData"];
 
-        $statement = mysqli_prepare($con, "INSERT INTO LOSTPOST(LostPostTitleData, LostPostPlaceData, LostPostDateData, LostPostColorData, LostPostMoreInfoData, LostPostImgData) VALUES (?,?,?,?,?,?)");
-    mysqli_stmt_bind_param($statement, "ssssss"
-        ,$LostPostTitleData, $LostPostPlaceData, $LostPostDateData, $LostPostColorData, $LostPostMoreInfoData, $LostPostImgData);
+        $statement = mysqli_prepare($con, "INSERT INTO LOSTPOST(LostPostTitleData, LostPostCategoryData, LostPostLocalData, LostPostPlaceData, LostPostDateData, LostPostColorData, LostPostMoreInfoData, LostPostImgData) VALUES (?,?,?,?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "ssssssss"
+        ,$LostPostTitleData, $LostPostCategoryData, $LostPostLocalData, $LostPostPlaceData, $LostPostDateData, $LostPostColorData, $LostPostMoreInfoData, $LostPostImgData);
     mysqli_stmt_execute($statement);
  
     $response = array();
