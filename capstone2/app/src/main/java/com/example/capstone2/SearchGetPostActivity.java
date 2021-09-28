8d1e48c106f3e6ec4f454648bc7d6c456e4632c0
SemiKimm <semikimm.m@gmail.com>
2021-09-29 06:59:44 +0900

    [semi-commit] PostingActivity.java & PostingRequest.java & Posting.xml : Posting function LostPostActivity.java & lost_post.xml : 분실물 게시글 자세히보기 기능 GetPostActivity.java & get_post.xml : 습득물 게시글 자세히보기 기능 PostListActivity.java & PostRequest.java & post_list.xml & post_list_item.xml : Post List function and db data check SelectPostingActivity.java & select_posting.xml : select lostposting or getposting LostPostingActivity.java & LostPostingRequest.java & lost_posting.xml : 분실물 글 작성 기능 GetPostingActivity.java & GetPostingRequest.java & get_posting.xml : 습득물 글 작성 기능 SelectSearchPostActivity.java & select_search_post.xml : 하단바 search button 클릭시 렌더링, 검색하려는 물건이 분실물인지 습득물인지 선택 SearchGetPostActivity.java & search_get_post.xml : 습득물 검색 기능 SearchGetPostRequest.java : 습득물 검색 조건 데이터 디비로 넘기는 요청 SearchGetPostListActivity.java : SearchGetPostActivity 로부터 검색 조건에 해당하는 게시글 데이터 받아서 해당 목록 보여주는 기능 SearchLostPostActivity.java & search_lost_post.xml : 분실물 검색 기능 SearchLostPostRequest.java : 분실물 검색 조건 데이터 디비로 넘기는 요청 SearchLostPostListActivity.java : SearchLostPostActivity 로부터 검색 조건에 해당하는 게시글 데이터 받아서 해당 목록 보여주는 기능 MainGetPostRequest.java & MainLostPostRequest.java & activity_main.xml & main_get_list_item.xml & main_lost_list_item.xml : 홈화면에서 상단바의 지역 선택 변경하였을때 해당 지역에 대한 게시글 목록 보이게 하는 기능 CategoryLostListActivity.java & CategoryLostListRequest.java : 홈화면에서 카테고리의 분류를 선택하면 해당 분류의 게시글 목록을 보여주는 기능 -------------------------------------- [change] X

--- capstone2/app/src/main/java/com/example/capstone2/SearchGetPostActivity.java
+++ capstone2/app/src/main/java/com/example/capstone2/SearchGetPostActivity.java
@@ -30,8 +30,8 @@ import java.util.HashMap;
 public class SearchGetPostActivity extends AppCompatActivity {
     private TextView date, color, category, local;
 
-    String[] colorItems = {"선택","검정색 ","흰색","빨강색","연두색","파랑색 ","노랑색","핑크색","보라색","회색","갈색"};
-    String[] categoryItems = {"선택","가방", "의류", "전자제품", "악세서리", "모자", "신발", "시계", "휴대폰"};
+    String[] colorItems = {"선택", "검정색 ","흰색","빨강색","연두색","파랑색 ","노랑색","핑크색","보라색","회색","갈색"};
+    String[] categoryItems = {"선택", "가방", "의류", "전자제품", "악세서리", "모자", "신발", "시계", "휴대폰"};
     String[] localItems = {"선택", "서울특별시", "강원도", "경기도", "경상남도", "경상북도", "광주광역시", "대구광역시"
             , "대전광역시", "부산광역시", "울산광역시", "인천광역시", "전라남도", "전라북도", "충청남도", "충청북도"
             , "제주특별자치도", "세종특별자치시", "기타"};
