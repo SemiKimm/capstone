a0a5b63c64964467a5adca6afd68f6f93465004c
SemiKimm <semikimm.m@gmail.com>
2021-09-29 04:45:46 +0900

    [semi-commit] PostingActivity.java & PostingRequest.java & Posting.xml : Posting function LostPostActivity.java & lost_post.xml : 분실물 게시글 자세히보기 기능 GetPostActivity.java & get_post.xml : 습득물 게시글 자세히보기 기능 PostListActivity.java & PostRequest.java & post_list.xml & post_list_item.xml : Post List function and db data check SelectPostingActivity.java & select_posting.xml : select lostposting or getposting LostPostingActivity.java & LostPostingRequest.java & lost_posting.xml : 분실물 글 작성 기능 GetPostingActivity.java & GetPostingRequest.java & get_posting.xml : 습득물 글 작성 기능 SelectSearchPostActivity.java & select_search_post.xml : 하단바 search button 클릭시 렌더링, 검색하려는 물건이 분실물인지 습득물인지 선택 SearchGetPostActivity.java & search_get_post.xml : 습득물 검색 기능 SearchGetPostRequest.java : 습득물 검색 조건 데이터 디비로 넘기는 요청 SearchGetPostListActivity.java : SearchGetPostActivity 로부터 검색 조건에 해당하는 게시글 데이터 받아서 해당 목록 보여주는 기능 SearchLostPostActivity.java & search_lost_post.xml : 분실물 검색 기능 SearchLostPostRequest.java : 분실물 검색 조건 데이터 디비로 넘기는 요청 SearchLostPostListActivity.java : SearchLostPostActivity 로부터 검색 조건에 해당하는 게시글 데이터 받아서 해당 목록 보여주는 기능 MainGetPostRequest.java & MainLostPostRequest.java & activity_main.xml & main_get_list_item.xml & main_lost_list_item.xml : 홈화면에서 상단바의 지역 선택 변경하였을때 해당 지역에 대한 게시글 목록 보이게 하는 기능 -------------------------------------- [change] MainGetPostRequest.java & MainLostPostRequest.java & activity_main.xml & main_get_list_item.xml & main_lost_list_item.xml : 홈화면 지역별 게시글 목록 처리 완료

--- capstone2/app/src/main/java/com/example/capstone2/MainActivity.java
+++ capstone2/app/src/main/java/com/example/capstone2/MainActivity.java
@@ -1,23 +1,82 @@
 package com.example.capstone2;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     String[] localItems = {"지역 선택", "서울특별시", "강원도", "경기도", "경상남도", "경상북도", "광주광역시", "대구광역시"
             , "대전광역시", "부산광역시", "울산광역시", "인천광역시", "전라남도", "전라북도", "충청남도", "충청북도"
             , "제주특별자치도", "세종특별자치시", "기타"};
     String selectedLocal;
+
+    //습득물 목록 관련 데이터
+    private static String TAG = "getJsonList";
+    private static final String TAG_GETJSON="getpostdata";
+    private static final String TAG_GetPostTitle = "GetPostTitleData";
+    private static final String TAG_GetPostCategory = "GetPostCategoryData";
+    private static final String TAG_GetPostLocal = "GetPostLocalData";
+    private static final String TAG_GetPostPlace = "GetPostPlaceData";
+    private static final String TAG_GetPostDate ="GetPostDateData";
+    private static final String TAG_GetPostColor ="GetPostColorData";
+    private static final String TAG_GetPostMoreInfo ="GetPostMoreInfoData";
+    private static final String TAG_GetPostImg ="GetPostImgData";
+    //분실물 목록 관련 데이터
+    private static final String TAG_LOSTJSON="lostpostdata";
+    private static final String TAG_LostPostTitle = "LostPostTitleData";
+    private static final String TAG_LostPostCategory = "LostPostCategoryData";
+    private static final String TAG_LostPostLocal ="LostPostLocalData";
+    private static final String TAG_LostPostPlace = "LostPostPlaceData";
+    private static final String TAG_LostPostDate ="LostPostDateData";
+    private static final String TAG_LostPostColor ="LostPostColorData";
+    private static final String TAG_LostPostMoreInfo ="LostPostMoreInfoData";
+    private static final String TAG_LostPostImg ="LostPostImgData";
+
+    ArrayList<HashMap<String, String>> mlostArrayList,mgetArrayList;
+    GridView mlostlistView,mgetlistView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mgetlistView = (GridView) findViewById(R.id.get_main_list);
+        mlostlistView = (GridView) findViewById(R.id.lost_main_list);
 
         Button getListCheckBtn=findViewById(R.id.get_list_check_btn);
         getListCheckBtn.setOnClickListener(view -> {
@@ -45,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
 
         //localSpinner 처리 (물건분류선택처리)
         Spinner localSpin = (Spinner)findViewById(R.id.selectLocalSpinner);
-        //local=findViewById(R.id.searchGetLocalData);
         ArrayAdapter<String> localAdapter=new ArrayAdapter<String>(
                 this,android.R.layout.simple_spinner_item,localItems
         );
@@ -55,6 +113,121 @@ public class MainActivity extends AppCompatActivity {
             @Override//선택되면
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
                 selectedLocal=localItems[position];
+                Log.e("localdata",selectedLocal);
+
+                Response.Listener<String> getresponseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject JsonObject = new JSONObject(response);
+                            boolean success=JsonObject.getBoolean("success");
+                            JSONArray jsonArray = JsonObject.getJSONArray("getpostdata");
+                            Log.e("getlist",String.valueOf(JsonObject));
+                            mgetArrayList = new ArrayList<>();
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject item = jsonArray.getJSONObject(i);
+
+                                String GetPostTitleData = item.getString(TAG_GetPostTitle );
+                                String GetPostCategoryData = item.getString(TAG_GetPostCategory );
+                                String GetPostLocalData = item.getString(TAG_GetPostLocal );
+                                String GetPostPlaceData = item.getString(TAG_GetPostPlace );
+                                String GetPostDateData = item.getString(TAG_GetPostDate );
+                                String GetPostColorData = item.getString(TAG_GetPostColor );
+                                String GetPostMoreInfoData = item.getString(TAG_GetPostMoreInfo );
+                                String GetPostImgData = item.getString(TAG_GetPostImg );
+
+                                HashMap<String,String> hashMap = new HashMap<>();
+
+                                hashMap.put(TAG_GetPostTitle, GetPostTitleData);
+                                hashMap.put(TAG_GetPostCategory, GetPostCategoryData);
+                                hashMap.put(TAG_GetPostLocal, GetPostLocalData);
+                                hashMap.put(TAG_GetPostPlace, GetPostPlaceData);
+                                hashMap.put(TAG_GetPostDate, GetPostDateData);
+                                hashMap.put(TAG_GetPostColor, GetPostColorData);
+                                hashMap.put(TAG_GetPostMoreInfo, GetPostMoreInfoData);
+                                hashMap.put(TAG_GetPostImg, GetPostImgData);
+                                mgetArrayList.add(hashMap);
+                                Log.e("gettest",String.valueOf(mgetArrayList));
+                            }
+                            if(success){
+                                ListAdapter adapter = new SimpleAdapter(
+                                        MainActivity.this, mgetArrayList, R.layout.main_get_list_item,
+                                        new String[]{TAG_GetPostTitle ,TAG_GetPostCategory ,TAG_GetPostLocal, TAG_GetPostDate},
+                                        new int[]{R.id.get_textView_list_title, R.id.get_textView_list_category, R.id.get_textView_list_local, R.id.get_textView_list_date}
+                                );
+                                mgetlistView.setAdapter(adapter);
+
+                            }else{
+                                Toast.makeText(getApplicationContext(),"검색 조회 실패",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            Log.e("test","test: catch");
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //서버로 Volley 이용해서 요청
+                MainGetPostRequest mainGetPostRequest = new MainGetPostRequest( selectedLocal, getresponseListener);
+                RequestQueue getqueue = Volley.newRequestQueue( MainActivity.this );
+                getqueue.add( mainGetPostRequest );
+
+                Response.Listener<String> lostresponseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject JsonObject = new JSONObject(response);
+                            boolean success=JsonObject.getBoolean("success");
+                            JSONArray jsonArray = JsonObject.getJSONArray("lostpostdata");
+                            Log.e("lostlist",String.valueOf(jsonArray));
+                            Log.e("jsonArray길이", String.valueOf(Integer.valueOf(jsonArray.length())));
+                            mlostArrayList = new ArrayList<>();
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject item = jsonArray.getJSONObject(i);
+
+                                String LostPostTitleData = item.getString(TAG_LostPostTitle );
+                                String LostPostCategoryData = item.getString(TAG_LostPostCategory );
+                                String LostPostLocalData = item.getString(TAG_LostPostLocal );
+                                String LostPostPlaceData = item.getString(TAG_LostPostPlace );
+                                String LostPostDateData = item.getString(TAG_LostPostDate );
+                                String LostPostColorData = item.getString(TAG_LostPostColor );
+                                String LostPostMoreInfoData = item.getString(TAG_LostPostMoreInfo );
+                                String LostPostImgData = item.getString(TAG_LostPostImg );
+
+                                HashMap<String,String> hashMap = new HashMap<>();
+
+                                hashMap.put(TAG_LostPostTitle, LostPostTitleData);
+                                hashMap.put(TAG_LostPostCategory, LostPostCategoryData);
+                                hashMap.put(TAG_LostPostLocal, LostPostLocalData);
+                                hashMap.put(TAG_LostPostPlace, LostPostPlaceData);
+                                hashMap.put(TAG_LostPostDate, LostPostDateData);
+                                hashMap.put(TAG_LostPostColor, LostPostColorData);
+                                hashMap.put(TAG_LostPostMoreInfo, LostPostMoreInfoData);
+                                hashMap.put(TAG_LostPostImg, LostPostImgData);
+                                mlostArrayList.add(hashMap);
+                                Log.e("losttest",String.valueOf(mlostArrayList));
+                            }
+                            if(success){
+                                ListAdapter adapter = new SimpleAdapter(
+                                        MainActivity.this, mlostArrayList, R.layout.main_lost_list_item,
+                                        new String[]{TAG_LostPostTitle , TAG_LostPostCategory, TAG_LostPostLocal, TAG_LostPostDate},
+                                        new int[]{R.id.lost_textView_list_title, R.id.lost_textView_list_category, R.id.lost_textView_list_local, R.id.lost_textView_list_date}
+                                );
+                                mlostlistView.setAdapter(adapter);
+                            }else{
+                                Toast.makeText(getApplicationContext(),"검색 조회 실패",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            Log.e("test","test: catch");
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //서버로 Volley 이용해서 요청
+                MainLostPostRequest mainLostPostRequest = new MainLostPostRequest( selectedLocal, lostresponseListener);
+                RequestQueue lostqueue = Volley.newRequestQueue( MainActivity.this );
+                lostqueue.add( mainLostPostRequest );
             }
             @Override//아무것도 선택 안되면
             public void onNothingSelected(AdapterView<?> parent) {
@@ -62,6 +235,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //습득물 목록 눌러서 해당 게시글 데이터 받아서 보내기
+        mgetlistView.setOnItemClickListener((AdapterView.OnItemClickListener) (adapterView, view, index, l) -> {
+            HashMap<String,String> data =(HashMap<String,String>) adapterView.getItemAtPosition(index);
+            Log.e("itemdata",String.valueOf(data));
+            String title=data.get(TAG_GetPostTitle);
+            String category=data.get(TAG_GetPostCategory);
+            String local = data.get(TAG_GetPostLocal);
+            String place = data.get(TAG_GetPostPlace);
+            String color = data.get(TAG_GetPostColor);
+            String date = data.get(TAG_GetPostDate);
+            String moreInfo=data.get(TAG_GetPostMoreInfo);
+            String imgUri=data.get(TAG_GetPostImg);
+            Intent postIntent = new Intent(MainActivity.this,GetPostActivity.class);
+            postIntent.putExtra("title",title);
+            postIntent.putExtra("category",category);
+            postIntent.putExtra("local",local);
+            postIntent.putExtra("place",place);
+            postIntent.putExtra("color",color);
+            postIntent.putExtra("date",date);
+            postIntent.putExtra("moreInfo",moreInfo);
+            postIntent.putExtra("imgUri",imgUri);
+            startActivity(postIntent);
+        });
 
+        //분실물 목록 눌러서 해당 게시글 데이터 받아서 보내기
+        mlostlistView.setOnItemClickListener((AdapterView.OnItemClickListener) (adapterView, view, index, l) -> {
+            HashMap<String,String> data =(HashMap<String,String>) adapterView.getItemAtPosition(index);
+            Log.e("itemdata",String.valueOf(data));
+            String title=data.get(TAG_LostPostTitle);
+            String category=data.get(TAG_LostPostCategory);
+            String local = data.get(TAG_LostPostLocal);
+            String place = data.get(TAG_LostPostPlace);
+            String color = data.get(TAG_LostPostColor);
+            String date = data.get(TAG_LostPostDate);
+            String moreInfo=data.get(TAG_LostPostMoreInfo);
+            String imgUri=data.get(TAG_LostPostImg);
+            Intent intent = new Intent(MainActivity.this,LostPostActivity.class);
+            intent.putExtra("title",title);
+            intent.putExtra("category",category);
+            intent.putExtra("local",local);
+            intent.putExtra("place",place);
+            intent.putExtra("color",color);
+            intent.putExtra("date",date);
+            intent.putExtra("moreInfo",moreInfo);
+            intent.putExtra("imgUri",imgUri);
+            startActivity(intent);
+        });
     }
 }
